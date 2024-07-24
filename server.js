2d390e0d28aac8066d26ba48bd0fd9e4054629aa
Adek1n <memeboi5789@gmail.com>
2024-07-24 11:53:25 +0800

    changed to switch statement

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
+require("dotenv").config();
 const e=require("http");
 const f=require("fs");
-console.log("Server Active");
+
 const app=e.createServer((req,res)=>{
-    if(req.url=="/"){
-        f.readFile("RESPONSE/index.html","utf8",(err,data)=>{
-            if(err){
-                console.error(err);
-            }
-            res.end(data);
-        })
-    }
-    else if(req.url=="/config.js"){
-        res.end(`const config={
-        MY_API_KEY:"${process.env.MY_API_KEY}"
-        }`);
-    }
-    else if(req.url=="/app.js"||req.url=="/styles.css"){
-        f.readFile(`RESPONSE/${req.url}`,"utf8",(err,data)=>{
-            if(err){
-                console.error(err);
-                console.log(req);
-            }
-            res.end(data);
-        })
-    }
-    else{
-        res.end("NOT FOUND");
+    const url=req.url;
+    switch(url){
+        case "/styles.css":
+            f.readFile(`RESPONSE/styles.css`,"utf8",(err,data)=>{
+                if(err){
+                    console.error(err);
+                }
+                res.end(data);
+            })
+            break;
+        case "/":
+            f.readFile("RESPONSE/index.html","utf8",(err,data)=>{
+                if(err){
+                    console.error(err);
+                }
+                res.end(data);
+            })
+            break;
+        case "/config.js":
+            res.end(`const config={
+                MY_API_KEY:"${process.env.MY_API_KEY}"
+                }`);
+            break;
+        case "/app.js":
+            f.readFile(`RESPONSE/app.js`,"utf8",(err,data)=>{
+                if(err){
+                    console.error(err);
+                }
+                res.end(data);
+            })
+            break;
+        default:
+            break;
     }
 });
-app.listen(3000);
+app.listen(process.env.port||3000);
